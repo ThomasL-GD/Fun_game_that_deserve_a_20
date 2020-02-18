20b4af6112dd18519b337a0ab6552f306fa7fd72
ThomasL-GD <59962750+ThomasL-GD@users.noreply.github.com>
2020-02-18 21:19:19 +0100

    First deck added + explanations
    A deck of 20 cards in working, it's an attack-based deck with mainly fire creatures
    Plus, there is an example of how to use it

--- Fun_game.c
+++ Fun_game.c
@@ -38,11 +38,33 @@ struct Card{
 };
 typedef struct Card card;
 
+
+
+
+
+
+void DeckDefinition (card deck[20]){
+	
+	for (int i = 0; i < 20; i++){
+		
+		strcat(deck[i].Entity.Name, deck[i].Element.Name);
+		deck[i].Entity.AtkPhy += deck[i].Element.ModifAtkPhy;
+		deck[i].Entity.AtkMag += deck[i].Element.ModifAtkMag;
+		deck[i].Entity.LifePhy += deck[i].Element.ModifLifePhy;
+		deck[i].Entity.LifeMag += deck[i].Element.ModifLifeMag;
+	
+	}
+	
+};
+
 int main(){
+	
+	srand(time(NULL));
+	
 	
 	element eau = {"'Eau",0,-1,1,1,0,"Soigne 1PV par tour aux autres creatures sous votre controle"};
 	element feu = {"e Feu",1,2,0,-1,0,"Inflige deux tours de brulure (-1PV par tour) pour toute attaque physique effectuée"};
-	element air = {"' Air",2,-1,0,1,1,"Subit 1 dégat magique de moins par attaque magique subie"};
+	element air = {"'Air",2,-1,0,1,1,"Subit 1 dégat magique de moins par attaque magique subie"};
 	element terre = {"e Terre",3,0,-1,2,0,"Subit 1 dégat physique de moins par attaque physique subie"};
 	
 	element fumee = {"e Fumee",4,0,1,1,-1,"Inflige 1 degat a tous les adversaires par tour"};
@@ -63,20 +85,17 @@ int main(){
     entity BoucTemeraire = {"Bouc Temeraire d", 3, 3, 5, 5};              //attack (phy + spe)
     entity Aigle = {"Aigle d", 3, 4, 6, 2};                               //attack
 
-	card firstCard = {Aigle, air};
+	// Ce tableau contient 20 cartes, c'est donc un deck, /! Les cartes sont triées donc quand on piochera il faudra prendre une carte aléatoire parmi le deck
+	card deck[20] = {Loup,feu, Loup,terre, DragonDeKomodo,feu, DragonDeKomodo,terre, AraigneeCracheuse,feu, AraigneeCracheuse, terre, AraigneeCracheuse,air, CrocodileAffame,feu, CrocodileAffame,air, CrocodileAffame,eau, ChefAlpaga,feu, ChefAlpaga,air, Grenouille,eau, Grenouille,feu, Grenouille,air, TortueSonore,feu, TortueSonore,terre, Aigle,feu, Aigle,air, Aigle,terre};
+	// Cette fonction est necessaire pour appliquer les modifications des élements aux cartes
+	DeckDefinition(deck);
 
-	strcat(firstCard.Entity.Name, firstCard.Element.Name);
-	firstCard.Entity.AtkPhy += firstCard.Element.ModifAtkPhy;
-	firstCard.Entity.AtkMag += firstCard.Element.ModifAtkMag;
-	firstCard.Entity.LifePhy += firstCard.Element.ModifLifePhy;
-	firstCard.Entity.LifeMag += firstCard.Element.ModifLifeMag;
 
-	printf("\n%s  ",firstCard.Entity.Name);
-	printf("%d/",firstCard.Entity.AtkPhy);
-	printf("%d/",firstCard.Entity.AtkMag);
-	printf("%d/",firstCard.Entity.LifePhy);
-	printf("%d",firstCard.Entity.LifeMag);
+	// EXEMPLES DE L' UTILISATION DES DECKS
 
+	//Pour récupérer une carte il faut "deck1[rang voulu]"
+	//On peut ainsi afficher le nom de la 13ème carte du deck par exemple :
+	printf ("%s\n",deck[12].Entity.Name);
 
 
 	return 0;
